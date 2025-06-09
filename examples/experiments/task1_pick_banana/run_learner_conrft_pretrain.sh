05b5a4efa2c58e02615713b231e2c0476be8ebe8
Yuhui Chen <cedric_chen2000@qq.com>
2025-06-09 17:44:34 +0800

    Update run_learner_conrft_pretrain.sh

--- examples/experiments/task1_pick_banana/run_learner_conrft_pretrain.sh
+++ examples/experiments/task1_pick_banana/run_learner_conrft_pretrain.sh
@@ -3,7 +3,7 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
 python ../../train_conrft_octo.py "$@" \
     --exp_name=task1_pick_banana \
     --checkpoint_path=/root/online_rl/conrft/examples/experiments/task1_pick_banana/conrft\
-    --q_weight=0.0 \
+    --q_weight=0.1 \
     --bc_weight=1.0 \
     --demo_path=./demo_data/task1_pick_banana_30_demos.pkl \
     --pretrain_steps=20000 \
